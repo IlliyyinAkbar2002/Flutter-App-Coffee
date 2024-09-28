4f8cb229e4504e0df7ed7160674545bfa56ef625
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-09-28 21:28:05 +0700

    Create a form.

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,32 @@ class HelloWorld extends StatefulWidget {
 class _HelloWorldState extends State<HelloWorld> {
   // State Variable
   var name = "";
+  var age = "";
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('Hello $name!'),
-        TextField(
-          onChanged: (value) => setState(() => name = value),
+        Text('Hello $name! My age is $age'),
+        Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: "Enter your name",
+            ),
+            onChanged: (value) => setState(() => name = value),
+          ),
         ),
+        Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your age',
+            ),
+            onChanged: (value) => setState(() => age = value),
+          ),
+        )
       ],
     );
   }
