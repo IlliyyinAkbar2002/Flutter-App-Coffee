edca114eef07031822b274f1b15c22c157332884
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-10-02 18:57:40 +0700

    Create a Cart, and make listview.

--- lib/offerspage.dart
+++ lib/offerspage.dart
@@ -5,7 +5,26 @@ class OffersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Offer(title: 'Coffee Master', description: 'Description 1');
+    return ListView(
+      children: const [
+        Offer(
+          title: 'Coffee Master',
+          description: 'Description 1',
+        ),
+        Offer(
+          title: 'Coffee Intermediate',
+          description: 'Description 2',
+        ),
+        Offer(
+          title: 'Coffee Beginner',
+          description: 'Description 3',
+        ),
+        Offer(
+          title: 'Coffee Beginner',
+          description: 'Description 3',
+        ),
+      ],
+    );
   }
 }
 
@@ -16,21 +35,49 @@ class Offer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.titleLarge,
+    return SizedBox(
+      height: 300,
+      child: Card(
+        color: Colors.amberAccent.shade400,
+        elevation: 10,
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color: Colors.amber.shade50,
+                    child: Text(
+                      title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                      selectionColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color: Colors.amber.shade50,
+                    child: Text(
+                      description,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        Center(
-          child: Text(
-            description,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
